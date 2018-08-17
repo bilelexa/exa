caf8275ad69061eee8870a7a9c6b323e736d349e
bilelexa <bilelexa>
2018-08-17 20:59:25 +0100

    second commit

--- src/main/java/com/exa/gestcmd/web/rest/CommandeResource.java
+++ src/main/java/com/exa/gestcmd/web/rest/CommandeResource.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class CommandeResource {
 
     private final CommandeRepository commandeRepository;
--- src/main/java/com/exa/gestcmd/web/rest/Detail_commandeResource.java
+++ src/main/java/com/exa/gestcmd/web/rest/Detail_commandeResource.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class Detail_commandeResource {
 
     private final Detail_commandeRepository detail_commandeRepository;
